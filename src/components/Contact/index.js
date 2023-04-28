b23ccab1d3bbed1b8f7192a3687613e0cf0a83f5
Sealcakes <taylorcseal@gmail.com>
2023-04-28 17:15:09 -0500

    adds emailjs configuration so that emails are successfully sent, noticed bug with cube spinner, will probably replace it

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,40 @@
 import Loader from 'react-loaders';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters/index';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Stars from '../Stars';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
+    const refForm = useRef();
 
     useEffect(() => {
         setTimeout(() => {
           setLetterClass('text-animate-hover')
         }, 3000)
     }, [])
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+                'service_h1qbask',
+                'template_5tk58yb',
+                refForm.current,
+                '_YhMfD5IJfuA1xY4w'
+            )
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false);
+                },
+                () => {
+                    alert('Failed to send message, please try again!')
+                }
+            )
+    }
 
     return (
         <>
@@ -31,13 +54,13 @@ const Contact = () => {
                         using the form below either.
                     </p>
                     <div className='contact-form'>
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'> 
-                                    <input type='text' name='name' placeholder='Name' required />
+                                    <input type='text' name='from_name' placeholder='Name' required />
                                 </li>
                                 <li className='half'> 
-                                    <input type='email' name='email' placeholder='Email' required />
+                                    <input type='email' name='user_email' placeholder='Email' required />
                                 </li>
                                 <li> 
                                     <input type='text' name='subject' placeholder='Subject' required />
