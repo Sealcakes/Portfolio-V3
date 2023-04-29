f20a2ba5b64bd64d13b7f9f6ca19f7eb5741de6e
Sealcakes <taylorcseal@gmail.com>
2023-04-29 12:48:37 -0500

    changes stars code to have more groups that flash at different intervals"

--- src/components/Stars/index.js
+++ src/components/Stars/index.js
@@ -2,43 +2,80 @@ import React, { useState, useEffect } from 'react';
 import './index.scss';
 
 const Stars = () => {
-  const [starsEven, setStarsEven] = useState([]);
-  const [starsOdd, setStarsOdd] = useState([]);
+  const [starsGroup1, setStarsGroup1] = useState([]);
+  const [starsGroup2, setStarsGroup2] = useState([]);
+  const [starsGroup3, setStarsGroup3] = useState([]);
+  const [starsGroup4, setStarsGroup4] = useState([]);
+  const [starsGroup5, setStarsGroup5] = useState([]);
 
   useEffect(() => {
-    const newStarsOdd = [];
-    const newStarsEven = [];
+    const newStarsGroup1 = [];
+    const newStarsGroup2 = [];
+    const newStarsGroup3 = [];
+    const newStarsGroup4 = [];
+    const newStarsGroup5 = [];
+
     for (let i = 0; i < 50; i++) {
       const star = {
         id: i,
         x: Math.random() * window.innerWidth,
         y: Math.random() * window.innerHeight,
       };
-      if (star.id % 2) {
-        newStarsEven.push(star);
+      if (star.id <= 10) {
+        newStarsGroup1.push(star);
+      } else if (star.id <= 20) {
+        newStarsGroup2.push(star);
+      } else if (star.id <= 30) {
+        newStarsGroup3.push(star);
+      } else if (star.id <= 40) {
+        newStarsGroup4.push(star);
       } else {
-        newStarsOdd.push(star);
+        newStarsGroup5.push(star);
       }
       
     }
-    setStarsEven(newStarsEven);
-    setStarsOdd(newStarsOdd);
+    setStarsGroup1(newStarsGroup1);
+    setStarsGroup2(newStarsGroup2);
+    setStarsGroup3(newStarsGroup3);
+    setStarsGroup4(newStarsGroup4);
+    setStarsGroup5(newStarsGroup5);
   }, []);
 
   return (
     <div>
-      {starsEven.map((evenStar) => (
+      {starsGroup1.map((group1) => (
         <div
-          key={evenStar.id}
-          className="even-stars"
-          style={{ top: evenStar.y, left: evenStar.x }}
+          key={group1.id}
+          className="group1-stars"
+          style={{ top: group1.y, left: group1.x }}
         />
       ))}
-      {starsOdd.map((oddStar) => (
+      {starsGroup2.map((group2) => (
         <div
-          key={oddStar.id}
-          className="odd-stars"
-          style={{ top: oddStar.y, left: oddStar.x }}
+          key={group2.id}
+          className="group2-stars"
+          style={{ top: group2.y, left: group2.x }}
+        />
+      ))}
+      {starsGroup3.map((group3) => (
+        <div
+          key={group3.id}
+          className="group3-stars"
+          style={{ top: group3.y, left: group3.x }}
+        />
+      ))}
+      {starsGroup4.map((group4) => (
+        <div
+          key={group4.id}
+          className="group4-stars"
+          style={{ top: group4.y, left: group4.x }}
+        />
+      ))}
+      {starsGroup5.map((group5) => (
+        <div
+          key={group5.id}
+          className="group5-stars"
+          style={{ top: group5.y, left: group5.x }}
         />
       ))}
     </div>
